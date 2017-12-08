f893311d221043e32f35ab2ded5ecb889045200f
David Roman <davidroman96@gmail.com>
2017-12-08 12:31:50 +0100

    Don't load feeds concurrently

--- main.go
+++ main.go
@@ -123,10 +123,12 @@ func eventLoop(s *ui.Screen, cfg backend.Config) {
 
 func loadFeeds(s *ui.Screen, db *scribble.Driver, cfg backend.Config) {
 	for _, f := range cfg.Feeds {
-		go func(f backend.ConfFeed) {
-			loadFeed(db, f, cfg.UpdateStartup)
-			s.Redraw(&feedManager)
-		}(f)
+		//go func(f backend.ConfFeed) {
+		//	loadFeed(db, f, cfg.UpdateStartup)
+		//	s.Redraw(&feedManager)
+		//}(f)
+		loadFeed(db, f, cfg.UpdateStartup)
+		s.Redraw(&feedManager)
 	}
 }
 
