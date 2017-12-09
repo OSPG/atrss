1e61bdb0b7846be161a905d3ec87cbf261eb9120
David Roman <davidroman96@gmail.com>
2017-12-09 01:00:25 +0100

    Move event loop to ui package

--- aux_funcs.go
+++ aux_funcs.go
@@ -2,18 +2,9 @@ package main
 
 import (
 	"log"
-	"os/exec"
 	"os/user"
 	"strings"
 )
-
-func OpenURL(browser, url string) {
-	cmd := exec.Command(browser, url)
-	err := cmd.Start()
-	if err != nil {
-		log.Println("Can not execute command: ", err)
-	}
-}
 
 func Expand(path string) string {
 	usr, err := user.Current()
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/gdamore/tcell"
 	scribble "github.com/nanobox-io/golang-scribble"
 
 	"github.com/OSPG/atrss/backend"
@@ -18,112 +17,6 @@ var feedManager feed.Manager
 func check(err error) {
 	if err != nil {
 		panic(err)
-	}
-}
-
-func eventLoop(s *ui.Screen, cfg backend.Config) {
-	for {
-		ev := s.PollEvent()
-
-		x, y := s.GetCursor()
-		switch ev := ev.(type) {
-		case *tcell.EventKey:
-			switch ev.Key() {
-			case tcell.KeyEscape, tcell.KeyCtrlC, tcell.KeyCtrlQ:
-				return
-			case tcell.KeyCtrlO:
-				if x == s.ItemsColumn {
-					f := feedManager.Get(ui.FeedIdx)
-					item := f.GetUnreadItem(y)
-					OpenURL(cfg.Browser, item.Link)
-					f.ReadItem(item)
-				}
-			case tcell.KeyDown:
-				_, h := s.GetSize()
-				if x == s.ItemsColumn {
-					f := feedManager.Get(ui.FeedIdx)
-					if f.Unread == 0 {
-						y = ui.FeedIdx
-						x = 0
-					} else if y < cfg.Layout.BoxHeigh-1 && uint32(y) < f.Unread-1 {
-						y++
-					}
-				} else if y < h-1 && y < feedManager.LenVisible()-1 {
-					y++
-				}
-				s.SetCursor(x, y)
-			case tcell.KeyUp:
-				if y > 0 {
-					y--
-					s.SetCursor(x, y)
-				}
-			case tcell.KeyRight:
-				if x == 0 {
-					ui.FeedIdx = y
-					s.SetCursor(s.ItemsColumn, 0)
-				}
-			case tcell.KeyLeft:
-				_, y := s.GetCursor()
-				y = ui.FeedIdx
-				s.SetCursor(0, y)
-			case tcell.KeyCtrlR:
-				feedManager.Update()
-			case tcell.KeyHome:
-				if x == s.ItemsColumn {
-					s.SetCursor(s.ItemsColumn, 0)
-				} else {
-					s.SetCursor(0, 0)
-				}
-			case tcell.KeyEnd:
-				if x == s.ItemsColumn {
-					f := feedManager.Get(ui.FeedIdx)
-
-					if f.Unread-1 > uint32(cfg.Layout.BoxHeigh) {
-						y = cfg.Layout.BoxHeigh - 1
-					} else {
-						y = int(f.Unread - 1)
-					}
-
-				} else {
-					x = 0
-					elements := feedManager.Len()
-					if elements > y {
-						_, h := s.GetSize()
-						y = h - 1
-					} else {
-						y = elements - 1
-					}
-				}
-				s.SetCursor(x, y)
-			}
-			switch ev.Rune() {
-			case ' ':
-				if x == s.ItemsColumn {
-					f := feedManager.Get(ui.FeedIdx)
-					if f.Unread != 0 {
-						item := f.GetUnreadItem(y)
-						f.ReadItem(item)
-					}
-				}
-			case 'o', 'O':
-				if x == s.ItemsColumn {
-					f := feedManager.Get(ui.FeedIdx)
-					item := f.GetUnreadItem(y)
-					OpenURL(cfg.Browser, item.Link)
-				}
-			case 'R':
-				f := feedManager.Get(ui.FeedIdx)
-				f.Update()
-			case ':':
-				if ui.StartCmdLine(s, &feedManager) {
-					return
-				}
-
-			}
-			//		case *tcell.EventResize:
-			//			printLayout(s)
-		}
-		s.Redraw(&feedManager)
 	}
 }
 
@@ -157,6 +50,6 @@ func main() {
 	s.SetCursor(0, 0)
 	s.Redraw(&feedManager)
 	loadFeeds(s, db, *cfg)
-	eventLoop(s, *cfg)
+	ui.StartEventLoop(s, &feedManager, *cfg)
 	saveFeeds(db)
 }
